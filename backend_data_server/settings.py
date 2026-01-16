47353e3e67af85d9134494c347222bdfdc04b521
Jairo Rodriguez <jairodri.a05@gmail.com>
2026-01-16 03:25:49 +0000

    guia 22 terminada

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "homepage",
     "rest_framework",
-    "demo_rest_api"
+    "demo_rest_api",
+    "firebase_admin",
+    "landing_api"
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,11 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
+
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CREDENTIALS_PATH, {
+   'databaseURL': 'https://landing-4a872-default-rtdb.firebaseio.com'
+})
---
+++ landing_api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# DRF
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Firebase Admin SDK
+from firebase_admin import db
+
+# Python
+from datetime import datetime
+
+
+class LandingAPI(APIView):
+    # Atributos solicitados
+    name = "Landing API"
+    collection_name = "votes"  # nombre del nodo/colección en Realtime Database
+
+    def get(self, request):
+
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+
+      # get: Obtiene todos los elementos de la col ección
+      data = ref.get()
+
+      # Devuelve un arreglo JSON
+      return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+
+      data = request.data
+
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+
+      current_time  = datetime.now()
+      custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+      data.update({"timestamp": custom_format })
+
+      # push: Guarda el objeto en la colección
+      new_resource = ref.push(data)
+
+      # Devuelve el id del objeto guardado
+      return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+
